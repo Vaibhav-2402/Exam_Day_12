a2ff819e4dea2d6f0ae2833ae455170b29f02060
VAIBHAV KUMAR <129760605+Vaibhav-2402@users.noreply.github.com>
2023-07-07 16:31:01 +0530

    Add files via upload

---
+++ charvar.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    char charvar = 'A';
+    char charvar1 = '\t';
+    cout<<charvar;
+    cout<<charvar1;
+    charvar = 'B';
+    charvar1 = '\n';
+    cout<<charvar;
+    cout<<charvar1;
+    return 0;
+}
